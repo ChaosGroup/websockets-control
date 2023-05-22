551521bf05595c9055208c8493674a8705e32fd3
Tsvetan Ovedenski <dev@tsovedenski.com>
2023-05-22 11:05:47 +0300

    Add comments

--- websockets-control-core/src/WebSocket/Control/Internal/ClientTable.hs
+++ websockets-control-core/src/WebSocket/Control/Internal/ClientTable.hs
@@ -9,6 +9,7 @@ import qualified Data.Time.Clock.System as SystemTime
 import qualified Network.WebSockets as WS
 
 
+-- |Opaque unique identifier for a connected client
 newtype ClientId = ClientId SystemTime.SystemTime
     deriving newtype (Eq, Ord)
 
@@ -16,24 +17,28 @@ instance Show ClientId where
     show (ClientId s) = show (SystemTime.systemSeconds s) <> "." <> show (SystemTime.systemNanoseconds s)
 
 
+-- |Generate a new client identifier
 newClientId :: IO ClientId
 newClientId = ClientId <$> SystemTime.getSystemTime
 
 
+-- |Connected client
 newtype Client = Client { clientConnection :: WS.Connection }
 
 
+-- |Message sent when client changes its state
 data ClientControl
     = Connected ClientId
     | Disconnected ClientId
 
 
+-- |Collection of connected clients
 data ClientTable = ClientTable
     { tblClients :: MVar (M.Map ClientId Client)
     , tblControl :: U.InChan ClientControl
     }
 
-
+-- |Create an empty table
 newClientTable :: IO ClientTable
 newClientTable = do
     tblClients <- newMVar mempty
@@ -41,6 +46,7 @@ newClientTable = do
     pure ClientTable {..}
 
 
+-- |Add new client to the table when the client connects
 addClient :: ClientTable -> WS.Connection -> IO (ClientId, U.OutChan ClientControl)
 addClient tbl conn = do
     !cid <- newClientId
@@ -50,6 +56,7 @@ addClient tbl conn = do
     pure (cid, outChan)
 
 
+-- |Remove the client from the table when the client disconnects
 removeClient :: ClientTable -> ClientId -> IO ()
 removeClient tbl cid = do
     removed <- modifyMVar (tblClients tbl) $ \m -> evaluate $ case M.lookup cid m of
@@ -58,6 +65,7 @@ removeClient tbl cid = do
     when removed $ U.writeChan (tblControl tbl) (Disconnected cid)
 
 
+-- |Get just the connection of all connected clients
 getConnections :: ClientTable -> IO [WS.Connection]
 getConnections tbl = do
     clients <- M.elems <$> readMVar (tblClients tbl)
